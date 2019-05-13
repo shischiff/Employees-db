118c503bd25500556ebe4ab4bd22e554fd0692f8
Shimron Schiff <shschiff@cisco.com>
2019-05-14 00:23:13 +0300

    adding unit test

---
+++ EmployeeDB/test/test.js
@@ -0,0 +1,12 @@
+var expect = require("chai").expect;
+var app = require("../app");
+var request = require("request");
+
+describe ("Basic Get", function() {
+    it ("returns 200 OK ", function (){
+        var url = "http://localhost:3000/employee/new"
+        request(url, function(err, res, body){
+            expect(res.statusCode).to.equal(200)
+        });
+    });
+});
