0d7c238550bfa437eb923790148527fb01998ce7
Shimron Schiff <shschiff@cisco.com>
2019-05-16 00:33:47 +0300

    fix test to actually fail

--- EmployeeDB/test/test.js
+++ EmployeeDB/test/test.js
@@ -2,11 +2,12 @@ var expect = require("chai").expect;
 var app = require("../app");
 var request = require("request");
 
-describe ("Basic Get", function() {
-    it ("returns 200 OK ", function (){
+describe ("Basic Get",function() {
+    it ("returns 200 OK ", function(done) {
         var url = "http://localhost:3000/employee/new"
-        request(url, function(err, res, body){
-            expect(res.statusCode).to.equal(201)
+        request(url, function(err,res,body){
+            expect(res.statusCode).to.equal(201);
+         done();
         });
     });
 });
